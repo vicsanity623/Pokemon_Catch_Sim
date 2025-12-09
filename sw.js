fa9d7845c28611707818dd0ff0c86471af194d75
Vic <125237471+vicsanity623@users.noreply.github.com>
2025-12-09 13:37:06 -0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'pokecatch-v0.0.1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install Event: Cache core assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event: Serve from cache, fallback to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached response if found
+      if (response) {
+        return response;
+      }
+      // Otherwise fetch from network
+      return fetch(event.request).then((networkResponse) => {
+        // Optional: Cache new requests (like Pokemon sprites) dynamically
+        // We only cache valid responses (status 200) and basic get requests
+        if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic' && networkResponse.type !== 'cors') {
+          return networkResponse;
+        }
+
+        // Clone response to put in cache
+        const responseToCache = networkResponse.clone();
+        
+        caches.open(CACHE_NAME).then((cache) => {
+          // We can cache external sprites here
+          cache.put(event.request, responseToCache);
+        });
+
+        return networkResponse;
+      });
+    })
+  );
+});
+
+// Activate Event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
