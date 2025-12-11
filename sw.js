b54b5120aa661172017f99f6fd3e39b812c8b635
Vic <125237471+vicsanity623@users.noreply.github.com>
2025-12-10 23:35:23 -0700

    Update CACHE_NAME to version 1.0.0.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pokecatch-v1.0.0.3'; // Change this string to force update next time (e.g., v10)
+const CACHE_NAME = 'pokecatch-v1.0.0.4'; // Change this string to force update next time (e.g., v10)
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
