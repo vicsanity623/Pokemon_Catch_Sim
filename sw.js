b3cd16ce394d3626a25b0478b2194869a2458496
Vic <125237471+vicsanity623@users.noreply.github.com>
2025-12-11 01:29:29 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'pokecatch-v1.0.0.4'; // Change this string to force update next time (e.g., v10)
+const CACHE_NAME = 'pokecatch-v1.0.0.5'; // Change this string to force update next time (e.g., v10)
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './pokemon.js',
   './explore.js',
+  './daily.js',
   './manifest.json'
 ];
 
